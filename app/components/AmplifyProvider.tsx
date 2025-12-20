fd539979adfc436e51c2c3ceee12e2b3de3a802a
Jeremy-Ramirez <jerarami@espol.edu.ec>
2025-12-20 16:44:53 -0500

    feat: Add AmplifyProvider component for AWS Cognito authentication and UI wrapper.

--- app/components/AmplifyProvider.tsx
+++ app/components/AmplifyProvider.tsx
@@ -3,14 +3,12 @@ import { Amplify } from "aws-amplify";
 import { Authenticator } from "@aws-amplify/ui-react";
 import "@aws-amplify/ui-react/styles.css"; // Estilos por defecto
 
+import { AppBar, Toolbar, Typography, Button, Box } from "@mui/material";
+import LogoutIcon from "@mui/icons-material/Logout";
+
 const userPoolId = process.env.NEXT_PUBLIC_AWS_COGNITO_USER_POOL_ID;
 const userPoolClientId =
   process.env.NEXT_PUBLIC_AWS_COGNITO_USER_POOL_CLIENT_ID;
-
-console.log("Configuring Amplify with:", {
-  userPoolId: userPoolId ? "Defined" : "Undefined",
-  userPoolClientId: userPoolClientId ? "Defined" : "Undefined",
-});
 
 if (userPoolId && userPoolClientId) {
   Amplify.configure({
@@ -35,11 +33,24 @@ export default function AuthWrapper({
   return (
     <Authenticator>
       {({ signOut, user }) => (
-        <main>
-          {/* Pasamos el usuario y signOut a los hijos si es necesario */}
-          {children}
-          <button onClick={signOut}>Sign Out</button>
-        </main>
+        <Box sx={{ flexGrow: 1 }}>
+          <AppBar position="static">
+            <Toolbar>
+              <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                Insightt
+              </Typography>
+              <Button
+                variant="contained"
+                color="error"
+                startIcon={<LogoutIcon />}
+                onClick={signOut}
+              >
+                Sign Out
+              </Button>
+            </Toolbar>
+          </AppBar>
+          <main>{children}</main>
+        </Box>
       )}
     </Authenticator>
   );
